d4b39bb24985648f9059fb52c894e037eff98f5f
Isaac Idibia <idibiaisaac3@gmail.com>
2023-04-04 16:26:04 +0100

    updated social media

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,6 +1,11 @@
+"use client";
 import Container from "@/components/Container";
 import Header from "@/components/Header";
 import React, { Fragment } from "react";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import { Mail, Telegram, WhatsApp } from "@mui/icons-material";
 
 const About = () => {
   return (
@@ -8,8 +13,8 @@ const About = () => {
       <Header />
       <main>
         <section id="backdrop">
-          <Container className="py-20 px-5 text-white flex flex-col gap-20 justify-between">
-            <div className="flex flex-col gap-10 w-[40%]">
+          <Container className="py-20 px-5 text-white flex flex-col gap-10 justify-between">
+            <div className="flex flex-col gap-5 w-[45%]">
               <div>
                 <h2 className="underline text-4xl font-bold mb-3">ABOUT</h2>
                 <a href="mailto:johndoe@gmail.com" className="text-lg">
@@ -25,14 +30,36 @@ const About = () => {
                   eu, pretium quis, sem. Nulla consequat massa quis enim. Donec
                   pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.
                   In enim justo, rhoncus ut, imperdiet a, venenatis vitae,
-                  justo. Nullam dictum felis eu pede mollis pretium. Integer
-                  tincidunt. Cras dapibus. Vivamus elementum semper nisi.
+                  justo.
                 </p>
               </div>
             </div>
             <div className="text-lg relative flex flex-col items-center w-[40%]">
               <p>Kaduna</p>
               <p className="address pl-10">No. 23 Kaduna Close</p>
+            </div>
+            <div className="flex mt-4 gap-4">
+              <a href="#" target="_blank">
+                <TwitterIcon className="w-8 h-8" />
+              </a>
+              <a href="#" target="_blank">
+                <GitHubIcon className="w-8 h-8" />
+              </a>
+              <a href="#" target="_blank">
+                <LinkedInIcon className="w-8 h-8" />
+              </a>
+              <a href="#" target="_blank">
+                <Mail className="w-8 h-8" />
+              </a>
+              <a href="#" target="_blank">
+                <WhatsApp className="w-8 h-8" />
+              </a>
+              <a href="#" target="_blank">
+                <Telegram className="w-8 h-8" />
+              </a>
+              {/* <a href="#" target="_blank">
+                  <FiberDvrRounded className="w-8 h-8" />
+                </a> */}
             </div>
           </Container>
         </section>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,14 @@ import Link from "next/link";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import { Mail, Telegram, WhatsApp } from "@mui/icons-material";
 
 export default function Home() {
   return (
     <Fragment>
       <Header />
       <main>
-        <section id="backdrop" className="relative">
+        <section id="backdrop">
           <Container className="py-20 px-5 h-full text-white flex flex-col justify-center">
             <div>
               <h1 className="text-6xl">
@@ -21,6 +22,29 @@ export default function Home() {
                 <span className="font-black">MIKE</span>
               </h1>
               <p className="tracking-[.25em] text-xl">Web Developer</p>
+              <div className="flex mt-4 gap-4">
+                <a href="#" target="_blank">
+                  <TwitterIcon className="w-8 h-8" />
+                </a>
+                <a href="#" target="_blank">
+                  <GitHubIcon className="w-8 h-8" />
+                </a>
+                <a href="#" target="_blank">
+                  <LinkedInIcon className="w-8 h-8" />
+                </a>
+                <a href="#" target="_blank">
+                  <Mail className="w-8 h-8" />
+                </a>
+                <a href="#" target="_blank">
+                  <WhatsApp className="w-8 h-8" />
+                </a>
+                <a href="#" target="_blank">
+                  <Telegram className="w-8 h-8" />
+                </a>
+                {/* <a href="#" target="_blank">
+                  <FiberDvrRounded className="w-8 h-8" />
+                </a> */}
+              </div>
               <div className="flex gap-6 items-center mt-8">
                 <Link
                   href="/resume"
@@ -35,17 +59,6 @@ export default function Home() {
                   Portfolio
                 </Link>
               </div>
-            </div>
-            <div className="flex gap-4 absolute bottom-10">
-              <a href="#" target="_blank">
-                <TwitterIcon className="w-8 h-8" />
-              </a>
-              <a href="#" target="_blank">
-                <GitHubIcon className="w-8 h-8" />
-              </a>
-              <a href="#" target="_blank">
-                <LinkedInIcon className="w-8 h-8" />
-              </a>
             </div>
           </Container>
         </section>
